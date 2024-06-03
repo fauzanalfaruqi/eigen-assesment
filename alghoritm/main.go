34814d5c1c7c9b644404cf8bcae0a590d7cb7dab
fauzanalfaruqi <fauzanalfaruqi@gmail.com>
2024-06-04 03:18:45 +0700

    Add alghoritm solution

--- alghoritm/main.go
+++ alghoritm/main.go
@@ -4,17 +4,21 @@ import (
 	"alghoritm/soal_1"
 	"alghoritm/soal_2"
 	"alghoritm/soal_3"
+	"alghoritm/soal_4"
+
 	"fmt"
 )
 
 func main() {
 	// Soal 1
 	word := "NEGIE1"
+
 	reversedWord := soal_1.ReverseWordWithNumber(word)
 	fmt.Println(reversedWord)
 
 	// Soal 2
 	const sentence = "Saya sangat senang mengerjakan soal algoritma"
+
 	longestWord := soal_2.Longest(sentence)
 	fmt.Printf("%s\n", longestWord)
 
@@ -24,4 +28,10 @@ func main() {
 
 	output := soal_3.CountOccurrences(INPUT, QUERY)
 	fmt.Println(output)
+
+	// Soal 4
+	matrix := [][]int{{1, 2, 0}, {4, 5, 6}, {7, 8, 9}}
+
+	result := soal_4.DiagonalDifference(matrix)
+	fmt.Println(result)
 }
